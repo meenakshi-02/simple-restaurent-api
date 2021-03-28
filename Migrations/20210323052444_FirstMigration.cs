684f1b20cb8fa5ddc01aae5feb29e3998fbdc8fe
meenakshi-02 <81479394+meenakshi-02@users.noreply.github.com>
2021-03-28 12:33:35 +0530

    Add files via upload

---
+++ Migrations/20210323052444_FirstMigration.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace restaurent.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "reference");
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                schema: "reference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(12)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuItems",
+                schema: "reference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FkCustomerId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    DateTime = table.Column<string>(type: "nvarchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Customers_FkCustomerId",
+                        column: x => x.FkCustomerId,
+                        principalSchema: "reference",
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "reference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FkCustomerId = table.Column<int>(type: "int", nullable: false),
+                    FkMenuItemId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    CreatedDateTime = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    servedDateTime = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    ReadyToServe = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_FkCustomerId",
+                        column: x => x.FkCustomerId,
+                        principalSchema: "reference",
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_MenuItems_FkMenuItemId",
+                        column: x => x.FkMenuItemId,
+                        principalSchema: "reference",
+                        principalTable: "MenuItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_FkCustomerId",
+                schema: "reference",
+                table: "Orders",
+                column: "FkCustomerId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_FkMenuItemId",
+                schema: "reference",
+                table: "Orders",
+                column: "FkMenuItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_FkCustomerId",
+                table: "Transactions",
+                column: "FkCustomerId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "reference");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "MenuItems",
+                schema: "reference");
+
+            migrationBuilder.DropTable(
+                name: "Customers",
+                schema: "reference");
+        }
+    }
+}
