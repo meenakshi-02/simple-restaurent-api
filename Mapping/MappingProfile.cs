3f452fed7e296bf81bf0e24d2bbf739686a14ad9
meenakshi-02 <81479394+meenakshi-02@users.noreply.github.com>
2021-03-28 12:33:20 +0530

    Add files via upload

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using restaurent.Models;
+using restaurent.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restaurent.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerReadDto>();
+            CreateMap<CustomerCreateDto, Customer>();
+            CreateMap<MenuItem, MenuItemReadDto>();
+            CreateMap<Order, OrderReadDto>();
+            CreateMap<OrderCreateDto, Order>();
+            CreateMap<Transaction, TransactionReadDto>();
+            CreateMap<TransactionCreateDto, Transaction>();
+        }
+    }
+}
