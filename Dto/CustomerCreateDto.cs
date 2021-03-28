2b26309578be7f73296fa8551d71bd4e56800862
meenakshi-02 <81479394+meenakshi-02@users.noreply.github.com>
2021-03-28 12:32:15 +0530

    Add files via upload

---
+++ Dto/CustomerCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restaurent.Dto
+{
+    public class CustomerCreateDto
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Dto/OrderCreateDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restaurent.Dto
+{
+    public class OrderCreateDto
+    {
+        public int FkCustomerId { get; set; }
+        public int FkMenuItemId { get; set; }
+        public int Quantity { get; set; }
+        public string CreatedDateTime { get; set; }
+        public string servedDateTime { get; set; }
+        public int Price { get; set; }
+        public bool Status { get; set; }
+        public bool ReadyToServe { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
